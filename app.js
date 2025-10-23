f4ac1937cea911942a253297685429000547227c
SYJBTECH <syjbtech@gmail.com>
2025-10-23 16:05:38 +0900

    SY initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello from AWS CI/CD with GitHub!");
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
+
